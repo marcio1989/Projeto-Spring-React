077b6a2f2b0aaadc01388c117748eb6797f61196
marcio1989 <marcio.ricardo.muniz@hotmail.com>
2021-05-03 16:54:14 -0300

    Componentes básico

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer mt-auto py-3 bg-dark">
+                <div className="container">
+                    <p className="text-light">App desenvolvido por <a href="https://github.com/marcio1989" target="_blank"
+                     rel="noreferrer">Marcio Ricardo</a></p>
+                    <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Linkedin do full stack: <a href="https://www.linkedin.com/in/marcio-muniz-920970a8/" 
+                    target="_blank" 
+                    rel="noreferrer">@Marcio Ricardo</a></small></p>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
