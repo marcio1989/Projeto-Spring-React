2612b552f605420bef1a5ec972f22baf9f537355
marcio1989 <marcio.ricardo.muniz@hotmail.com>
2021-05-10 12:27:43 -0300

    Atualizando o Dashboard da página

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -10,13 +10,13 @@ const Dashboard = () => {
             <NavBar />
             <div className="container">
                 <h1 className="text-primary py-3">Dashboard de vendas</h1>
-
+                    
                 <div className="row px-3">
-                    <div className="col-sm-6">
+                    <div className="col-6">
                         <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
                         <BarChart />
                     </div>
-                    <div className="col-sm-6">
+                    <div className="col-6">
                         <h5 className="text-center text-secondary">Todas vendas</h5>
                         <DonutChart />
                     </div>
