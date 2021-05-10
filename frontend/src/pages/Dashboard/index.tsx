98d82a387e32298693e120b865379a1a280df11d
marcio1989 <marcio.ricardo.muniz@hotmail.com>
2021-05-10 13:21:17 -0300

    Dashboard auto

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -12,11 +12,11 @@ const Dashboard = () => {
                 <h1 className="text-primary py-3">Dashboard de vendas</h1>
 
                 <div className="row px-3">
-                    <div className="col-sm-6">
+                    <div className="col-sm-auto">
                         <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
                         <BarChart />
                     </div>
-                    <div className="col-sm-6">
+                    <div className="col-sm-auto">
                         <h5 className="text-center text-secondary">Todas vendas</h5>
                         <DonutChart />
                     </div>
